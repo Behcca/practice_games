fb72b4ceb64eed90aaaeaf1baa5c4fb72502bf49
bpmediocre <bpresant@nextgenins.com>
2022-03-08 15:47:44 -0700

    update all tiles to be blank

--- tic-tac-toe/3tdiv.js
+++ tic-tac-toe/3tdiv.js
@@ -1,5 +1,5 @@
-function initializeTile() {
-    const tile = document.getElementById('1a');
+function initializeTile(id) {
+    const tile = document.getElementById(id);
     const originalInnerHTML = tile.innerHTML;
     tile.innerHTML = '_';
     console.log(tile.innerHTML);
@@ -9,6 +9,14 @@ function initializeTile() {
 document.addEventListener(
     "DOMContentLoaded",
     function() {
-        initializeTile();
+        initializeTile('1a');
+        initializeTile('1b');
+        initializeTile('1c');
+        initializeTile('2a');
+        initializeTile('2b');
+        initializeTile('2c');
+        initializeTile('3a');
+        initializeTile('3b');
+        initializeTile('3c');
     }
 );
